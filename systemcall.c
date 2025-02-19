947d25d7d39b9ba3fbc4b5c5cbe68cd8c842e04e
SREEHARI <sreehari14shr@gmail.com>
2025-02-19 17:37:24 +0530

    system calls for linux

---
+++ systemcall.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main(){
+    int pid;
+    pid=fork();
+    if(pid<0){
+        printf("fork failed");
+        exit(1);
+    }
+    else if(pid==0){
+        execlp("/bin/date","date",NULL);
+        exit(0);
+    }
+    else {
+        printf("process id is-%d\n",getpid());
+        wait(NULL);
+        exit(0);
+
+    }
+    return 0;
+}
