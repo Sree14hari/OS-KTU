c0eea55eb02ef7198ef4c2689414addeac4b48f9
SREEHARI <sreehari14shr@gmail.com>
2025-02-19 20:05:52 +0530

    Update unix.c

--- unix.c
+++ unix.c
@@ -9,3 +9,53 @@ printf("\n File st_uid %d\n",sfile.st_uid);
 printf("\n File st_size %ld\n",sfile.st_size);
 return 0;
 }
+
+
+//2nd one 
+#include<stdio.h>
+#include<dirent.h>
+#include<stdlib.h>
+struct dirent *dptr;
+
+int main()
+{
+char buff[256];
+DIR *dirp;
+printf("Enter directory name:");
+scanf("%s",buff);
+if((dirp=opendir(buff))==NULL)
+{
+printf("Error");
+exit(1);
+}
+while(dptr=readdir(dirp))
+{
+printf("%s\n",dptr->d_name);
+}
+closedir(dirp);
+return 0;
+}
+
+//3rd one 
+#include<stdio.h> 
+#include<dirent.h> 
+#include<stdlib.h> 
+struct dirent *dptr; 
+int main() 
+{ 
+char buff[256]; 
+DIR *dirp; 
+printf("Enter directory name:"); 
+scanf("%s",buff); 
+if((dirp=opendir(buff))==NULL) 
+{ 
+printf("Error"); 
+exit(1); 
+} 
+while(dptr=readdir(dirp)) 
+{ 
+printf("%s\n",dptr->d_name); 
+} 
+closedir(dirp); 
+return 0; 
+}
